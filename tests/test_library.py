c7cc59addeaeec039d42c44cfef0c9c326aa73f7
Enrico <168141917+Enrico-Alvarenga@users.noreply.github.com>
2025-09-24 20:57:33 -0300

    Update test_library.py

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,6 +1,19 @@
 from src.library import SistemaBiblioteca
 
+
 def test_status_inicial_do_sistema():
     """Testa se o sistema é inicializado corretamente."""
     sistema = SistemaBiblioteca()
     assert sistema.status() == "Sistema de Biblioteca Online!"
+
+
+def test_cadastrar_usuario():
+    """Testa o Caso de Uso 2: Cadastrar um novo usuário."""
+    sistema = SistemaBiblioteca()
+    sucesso = sistema.cadastrar_usuario(nome="Eduardo Cipolaro", ra="168797")
+    
+    assert sucesso is True
+    
+    usuario = sistema.consultar_usuario_por_ra("168797")
+    assert usuario is not None
+    assert usuario['nome'] == "Eduardo Cipolaro"
